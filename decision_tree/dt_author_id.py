bfb515cf5905f944584e6369ed5f397e6ba92af5
captain320 <yaqub320@gmail.com>
2020-02-27 16:48:38 +0900

    added decision tree algorithm

---
+++ decision_tree/dt_author_id.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+X = features_train
+Y = labels_train
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(X, Y)
+
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+
+print("Accuracy is ", acc)
+print(len(features_train[0]))
+
+#########################################################
+
+
